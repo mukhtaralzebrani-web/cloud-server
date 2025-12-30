864fa5650e0df874d50e4635b9c0217c588db883
mukhtaralzebrani-web <mukhtaralzebrani@gmail.com>
2025-12-31 00:28:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  res.end('<h1>السيرفر يعمل بنجاح على الإنترنت</h1>');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server is running");
+});
